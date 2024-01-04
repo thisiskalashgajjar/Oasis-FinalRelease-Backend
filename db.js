b5c342851ffd6b845aaf916de00419d98b92e152
thisiskalashgajjar <70331771+thisiskalashgajjar@users.noreply.github.com>
2024-01-04 17:44:18 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/crud?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+const mongoURI = "mongodb+srv://thisiskalashgajjar:ZGFb2ux5OerBjRGT@thisiskalashgajjar.orzvxh6.mongodb.net/?retryWrites=true&w=majority";
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
